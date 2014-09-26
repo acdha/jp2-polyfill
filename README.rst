40d780e28e149428b5facacced066840ab6f45d8
Chris Adams <chris@improbable.org>
2014-09-26 18:27:15 -0400

    Initial Release

---
+++ README.rst
@@ -0,0 +1,20 @@
+JPEG 2000 Polyfill
+==================
+
+Experiment to see how realistic it would be to offer JavaScript-decoded JP2 images for browsers without native
+support.
+
+Dependencies:
+
+* https://github.com/kripken/j2k.js
+
+Major TODO items
+----------------
+
+* Support for anything other than 8-bit RGB images
+* Implement <picture> support
+* Improved compatibility with existing element styles
+* Upgrade to OpenJPEG 2.x, Emscripten optimizations
+* Implement CSS background-image support
+* Exposed API or data- attribute-driven controls?
+* Support for WebP or JPEG XR
---
+++ jp2-polyfill.js
@@ -0,0 +1,138 @@
+/* global console, openjpeg */
+
+(function () {
+    'use strict';
+
+    function loadOpenJPEG() {
+        var s = document.createElement('script');
+
+        if (s.addEventListener) {
+            s.addEventListener('load', initPolyfill, false);
+        } else if (s.readyState) {
+            // IE 10 and worse:
+            s.onreadystatechange = function () {
+                if (s.readyState == 'loaded' || s.readyState == 'complete') {
+                    initPolyfill();
+                }
+            };
+        }
+
+        s.src = 'external/openjpeg.js';
+
+        var firstScript = document.getElementsByTagName('script')[0];
+        firstScript.parentNode.insertBefore(s, firstScript);
+    }
+
+    function initPolyfill() {
+        console.log('JPEG-2000 polyfill loading');
+
+        updateImageTags();
+
+        console.log('JPEG-2000 polyfill complete');
+    }
+
+    function updateImageTags() {
+        var imgs = document.querySelectorAll('img');
+        for (var i = 0; i < imgs.length; i++) {
+            var img = imgs[i];
+
+            // TODO: simply require a data- attribute or src ends with .jp2/j2k?
+
+            if (img.complete && img.naturalWidth !== undefined && img.naturalWidth > 0) {
+                console.log('Skipping loaded', img);
+                continue;
+            }
+
+            console.log(img, img.complete, img.naturalWidth);
+
+            var canvas = createCanvasFromUrl(img.src);
+            canvas.id = img.id;
+            canvas.className = img.className;
+
+            if (img.attributes.style) {
+                canvas.setAttribute('style', img.attributes.style.value);
+            }
+
+            if (img.style.display) {
+                canvas.style.display = img.style.display;
+            } else {
+                canvas.style.display = 'inline';
+            }
+
+            // FIXME: copy computed styles or punt outright?
+
+            // img.parentNode.insertBefore(canvas, img.nextSibling);
+            img.parentNode.replaceChild(canvas, img);
+        }
+    }
+
+    function createCanvasFromUrl(src) {
+        var canvas = document.createElement('canvas'),
+            xhr = new XMLHttpRequest(),
+            fileType = (src.indexOf('.j2k') > -1) ? 'j2k' : 'jp2';
+
+        xhr.open('GET', src);
+        xhr.responseType = 'arraybuffer';
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+
+            if (xhr.status !== 200 && xhr.status !== 0) {
+                throw "Unable to load image data: XHR status " + xhr.status + " for " + src;
+            }
+
+            var bytes = new Uint8Array(xhr.response),
+                imageRGB, imageRGBA,
+                startTime, endTime;
+
+            startTime = Date.now();
+            imageRGB = openjpeg(bytes, fileType);
+            endTime = Date.now();
+            console.log('Decode of %s took %dms', src, endTime - startTime);
+
+            canvas.width = imageRGB.width;
+            canvas.height = imageRGB.height;
+
+            var ctx = canvas.getContext('2d');
+
+            var pixelsPerChannel = imageRGB.width * imageRGB.height;
+            imageRGBA = ctx.createImageData(imageRGB.width, imageRGB.height);
+
+            // FIXME: handle alpha channel
+            // FIXME: handle different color depths and layouts
+
+            var i = 0, j = 0;
+            while (i < imageRGBA.data.length && j < pixelsPerChannel) {
+                imageRGBA.data[i] = imageRGB.data[j]; // R
+                imageRGBA.data[i + 1] = imageRGB.data[j + pixelsPerChannel]; // G
+                imageRGBA.data[i + 2] = imageRGB.data[j + (2 * pixelsPerChannel)]; // B
+                imageRGBA.data[i + 3] = 255; // A
+
+                // Next pixel
+                i += 4;
+                j += 1;
+            }
+
+            ctx.putImageData(imageRGBA, 0, 0);
+        };
+
+        xhr.send(null);
+        return canvas;
+    }
+
+    // There's no standard way to detect the image formats supported by the current browser
+    // so we'll create a test image and
+
+    var img = new Image();
+    img.onerror = img.load = function () {
+        if (!img.width || img.width === 0) {
+            console.log('Polyfill appears to be necessary:', img.width, img.height);
+            loadOpenJPEG();
+        }
+    };
+
+    // This is a 4px image because OpenJPEG does not appear to support 1px:
+    img.src = 'data:image/jp2;base64,AAAADGpQICANCocKAAAAFGZ0eXBqcDIgAAAAAGpwMiAAAAAtanAyaAAAABZpaGRyAAAABAAAAAQAAw8HAAAAAAAPY29scgEAAAAAABAAAABpanAyY/9P/1EALwAAAAAABAAAAAQAAAAAAAAAAAAAAAQAAAAEAAAAAAAAAAAAAw8BAQ8BAQ8BAf9SAAwAAAABAQAEBAAB/1wABECA/5AACgAAAAAAGAAB/5PP/BAQFABcr4CA/9k=';
+})();
+
